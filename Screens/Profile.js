dfe2eef4b34fee7d5c690368f458e336978a093c
imrohit007 <rohitthakur9830@gmail.com>
2021-05-08 11:00:41 +0530

    first commit

---
+++ Screens/Profile.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+
+function Profile() {
+    return (
+        <View style={{alignItems:'center', justifyContent:'center', flex:1}}>
+            <Text>Profile Screen</Text>
+        </View>
+    )
+}
+
+export default Profile
